4eb04d236a057a41ac288c9ac617d853c983d74a
Steven Lu <slu.true@gmail.com>
2019-07-18 09:38:50 -0700

    Progress made on serving images

--- logo-generator/app.py
+++ logo-generator/app.py
@@ -1,15 +1,21 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
+from io import BytesIO
+from image_generator import generate
+
 app = Flask(__name__)
 
-@app.route("/")
-def serve_react_app():
-    return "hello"
-    #return send_file("app/build/index.html")
+def send_pil_image(pil_img):
+    img_io = BytesIO()
+    pil_img.save(img_io, 'PNG', quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/png')
 
-@app.route("/static/<filename>")
-def serve_static(filename):
-    return send_file("./styles/" + filename)
-    #return send_file("./app/build/static" + filename)
+@app.route("/generate")
+def serve_image():
+    print(request.form)
+    image = generate()
+    return send_pil_image(image)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- logo-generator/image_generator.py
+++ logo-generator/image_generator.py
@@ -11,13 +11,13 @@ def trim(im):
         raise ValueError("cannot trim; image was empty")
 
 def generate():
-    font = ImageFont.truetype('./fonts/montserrat/Montserrat-Light.otf', size=600)
+    font = ImageFont.truetype('./fonts/montserrat/Montserrat-Light.otf', size=100)
     #font = ImageFont.truetype('./fonts/metropolis/Metropolis-Bold.otf', size=600)
     (x, y) = (0, 0) # x = 22 for the I in Roboto Mono font
     color = '#C0C0C0' # white color
     dotcolor = '#8B008B'
 
-    image = Image.new('RGBA', (10000, 10000), (255, 0, 0, 0))
+    image = Image.new('RGBA', (2000, 2000), (255, 0, 0, 0))
     draw = ImageDraw.Draw(image)
     message1 = 'Logodot'
     message2 = '.'
@@ -27,6 +27,4 @@ def generate():
     draw.text((width, y), message2, fill=dotcolor, font=font)
 
     image = trim(image)
-    image.save('./logo.png')
-
-generate()
+    return image
